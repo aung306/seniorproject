4536843fc1cc5491e1d85bd206626f84e5be8f6c
David Vallejo-Lozano <davidvallejolozano@hotmail.com>
2025-04-23 14:08:55 -0400

    Test inputs provided and Wav to MIDI cleaned up

--- MIDI_to_pdf.py
+++ MIDI_to_pdf.py
@@ -9,5 +9,6 @@ def midi_to_pdf(midi_file_path, output_pdf_path):
     except subprocess.CalledProcessError as e:
         print(f"MuseScore error: {e}")
 
-#midi_to_pdf('./input/input.mid', './output/output.pdf')
-midi_to_pdf('./input/input2.mid', './output/output2.pdf')
+#midi_to_pdf('./input/Twinkle_Twinkle.mid', './output/Twinkle_Twinkle_output.pdf')
+midi_to_pdf('./input/Sneaky Snitch.mid', './output/Sneaky Snitchoutput.pdf')
+#midi_to_pdf('./input/NC_music_vocals.mid', './output/NC_music_vocals_output.pdf')
